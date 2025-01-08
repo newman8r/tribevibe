137911034db299a64ba452dbdbac2539aac497eb
Stephen Newman <smnewman07@gmail.com>
2025-01-08 04:44:58 -0800

    remove dist folder from repo

--- backend/dist/entities/channel.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { User } from './user.entity';
-import { Message } from './message.entity';
-export declare class Channel {
-    id: string;
-    name: string;
-    users: User[];
-    messages: Message[];
-}
